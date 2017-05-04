5e7f8069d5cb1316337cd760dfecd901d0ba245c
Antonino Porcino <nino.porcino@gmail.com>
2017-05-04 13:16:48 +0200

    add macros

--- example-tag.html.jsx
+++
@@ -1,4 +0,0 @@
-import React = require("react");
-
-const render = (props: any, context) => (<div>{(()=>{let fortytwo=42; return ((()=>{let But1 = (props)=>(<span>Hello {fortytwo}</span>); return (<div><But1></But1></div>);})());})()}</div>);
-export = render;
---
+++ spec/macroSpec.ts
@@ -0,0 +1,39 @@
+﻿import { renderComponent as render } from "./makeInlineComponent";
+import { defaultOptions } from "../utils/options";
+
+describe("'macro' tag", ()=> {
+   it("allow to define macro component", ()=>{          
+      const template = `
+         <Test stateless export="require">
+            <ul><MyItem></MyItem></ul>
+         </Test>
+         <macro name="MyItem">
+            <li>hello</li>
+         </macro>`;     
+      const rendered = render(template);      
+      const expected = `<div><ul><li>hello</li></ul></div>`;
+      expect(rendered).toEqual(expected);      
+   });
+
+   // TODO works with if, scope, etc.
+   it("works with if", ()=>{          
+      const template = `
+         <Test stateless export="require">
+            <ul if="{{true}}"><MyItem></MyItem></ul>
+            <ul if="{{false}}"><MyItem></MyItem></ul>
+            <ul><MyItem if="{{false}}"></MyItem></ul>
+         </Test>
+         <macro name="MyItem">
+            <li>hello</li>
+         </macro>`;     
+      const rendered = render(template);      
+      const expected = `<div><ul><li>hello</li></ul><ul></ul></div>`;
+      expect(rendered).toEqual(expected);      
+   });
+
+   // macro def must have single direct child
+
+   // macro instance cannot contain children
+
+   // macro def cannot have attributes
+});
